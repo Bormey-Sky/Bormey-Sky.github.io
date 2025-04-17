3c6c616812d42b7f4dfc74e29eb14ed25b334e2b
Sky <79532409+Bormey-Sky@users.noreply.github.com>
2025-04-17 21:59:48 +0700

    added passion project page

---
+++ pages/passionProject.vue
@@ -0,0 +1,6 @@
+<template>
+
+<div id="passionProject" class="mt-12 -mb-18">
+        <Typewriter phrase="Passion Project" :typo="true" :restart="true"  class="min-h-[10rem] page-title !text-[220%] text-center" />
+    </div>
+</template>
